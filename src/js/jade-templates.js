f029090d6c0565980a4e85a2632cc49e2678723f
Jed Wood <jed@limechile.com>
2013-11-20 14:40:26 -0700

    updated font path. Made app list dynamic, pulling from Goog Spreadsheet

--- src/js/jade-templates.js
+++ src/js/jade-templates.js
@@ -151,3 +151,29 @@ buf.push("</ul><h6 class=\"fg-white\"><a href=\"companyprofile.html\" class=\"ge
 
 buf.push("</div>");;return buf.join("");
 }
+
+function receiverList_tpl(locals) {
+var buf = [];
+var locals_ = (locals || {}),receiverList = locals_.receiverList;// iterate receiverList
+;(function(){
+  var $$obj = receiverList;
+  if ('number' == typeof $$obj.length) {
+
+    for (var $index = 0, $$l = $$obj.length; $index < $$l; $index++) {
+      var app = $$obj[$index];
+
+buf.push("<li class=\"col-sm-4 app-rapper\"><a" + (jade.attrs({ 'href':(app.link), "class": [('text-center')] }, {"href":true})) + "><img" + (jade.attrs({ 'src':('img/apps/' + app.img), "class": [('app-icon')] }, {"src":true})) + "/></a><h5 class=\"fg-maqua\">" + (jade.escape(null == (jade.interp = app.name) ? "" : jade.interp)) + "</h5><div class=\"app-description\"><h6>" + (jade.escape(null == (jade.interp = app.description) ? "" : jade.interp)) + "</h6></div></li>");
+    }
+
+  } else {
+    var $$l = 0;
+    for (var $index in $$obj) {
+      $$l++;      var app = $$obj[$index];
+
+buf.push("<li class=\"col-sm-4 app-rapper\"><a" + (jade.attrs({ 'href':(app.link), "class": [('text-center')] }, {"href":true})) + "><img" + (jade.attrs({ 'src':('img/apps/' + app.img), "class": [('app-icon')] }, {"src":true})) + "/></a><h5 class=\"fg-maqua\">" + (jade.escape(null == (jade.interp = app.name) ? "" : jade.interp)) + "</h5><div class=\"app-description\"><h6>" + (jade.escape(null == (jade.interp = app.description) ? "" : jade.interp)) + "</h6></div></li>");
+    }
+
+  }
+}).call(this);
+;return buf.join("");
+}
