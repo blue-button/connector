dd8332528753fbc586dfc5c69b25ebd0b0212523
jedwood <jed@limechile.com>
2015-03-21 17:48:53 -0600

    tweaking the build listener. WIP

--- build-listener.js
+++ build-listener.js
@@ -1,7 +1,7 @@
 
 var exec  = require('child_process').exec;
 var config = require('nconf');
-config.argv().env().file({ file: __dirname + './config.json' }).defaults({'PORT':5000});
+config.argv().env().file({ file: __dirname + '/config.json' }).defaults({'PORT':5000});
 
 var express = require('express');
 var app = express();
@@ -10,36 +10,38 @@ app.get('/ping', function(req, res) {
   res.send("pong");
 });
 
-var leLog;
 app.get('/build', function (req, res) {
-  console.log('Building orgs...');
+  res.write('<html><head>');
+  res.write('<body>');
+  logIt('Building orgs...');
   exec('cd ' + config.get('MASTER_DIR') + '; git pull origin master; npm run build-organizations; npm run build-apps; git status;', function(error, stdout, stderr){
     if (/nothing to commit, working directory clean/.test(stdout)) {
       logIt("NOTHING TO DO HERE");
-      res.send(leLog);
-      leLog = '';
+      res.end('</body></html>');
     } else {
       logIt('Committing changes to master branch...');
+      logIt('cd ' + config.get('GHPAGES_DIR') + '; git pull origin gh-pages; cd ' + config.get('MASTER_DIR') + '; grunt dist; git add --all; git commit -am "update data for one or more profiles"; git push origin master;');
       exec('cd ' + config.get('GHPAGES_DIR') + '; git pull origin gh-pages; cd ' + config.get('MASTER_DIR') + '; grunt dist; git add --all; git commit -am "update data for one or more profiles"; git push origin master;', function(error, stdout, stderr){
+      // exec('cd ' + config.get('GHPAGES_DIR') + '; git pull origin gh-pages; cd ' + config.get('MASTER_DIR') + '; grunt dist; git status;', function(error, stdout, stderr){
         logIt(stdout);
         logIt('Committing changes to gh-pages branch...');
+        logIt('cd ' + config.get('GHPAGES_DIR') + '; git add --all; git commit -am "update data for one or more profiles"; git push origin gh-pages;')
         exec('cd ' + config.get('GHPAGES_DIR') + '; git add --all; git commit -am "update data for one or more profiles"; git push origin gh-pages;', function(error, stdout, stderr){
+        // exec('cd ' + config.get('GHPAGES_DIR') + '; git status;', function(error, stdout, stderr){
           logIt(stdout);
           logIt("DONE");
-          res.send(leLog);
-          leLog = '';
+          res.end('</body></html>')
         });
       });
     }
   });
 
+  function logIt(m) {
+    console.log(m);
+    res.write('<code>' + m + '</code>');
+  }
 });
 
-
-function logIt(m) {
-  console.log(m);
-  leLog += m + '/n';
-}
 
 // FIRE IT UP
 var server = app.listen(config.get('PORT'), function () {
