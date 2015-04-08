d68e896b5571c1745904d6d7dcbc1c887ce89474
jedwood <jed@limechile.com>
2015-04-08 08:08:58 -0600

    build config and gitignoring

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Build the site
+cd ../gh-pages;
+git pull origin gh-pages;
+cd ../connector;
 git pull origin master;
 npm run build-organizations;
 npm run build-apps;
 git status;
-cd ../gh-pages;
-git pull origin gh-pages;
-cd ../connector;
 grunt dist;
 git add --all;
 git commit -am "update data for one or more profiles";
