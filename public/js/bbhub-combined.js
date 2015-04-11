6d1d2f7dd317b0109aa81807dcd321e8e63ef82d
jedwood <jed@limechile.com>
2015-04-11 15:51:57 -0600

    remove unused jquery.cookie file

--- public/js/bbhub-combined.js
+++ public/js/bbhub-combined.js
@@ -20,113 +20,6 @@ if("undefined"==typeof jQuery)throw new Error("Bootstrap's JavaScript requires j
  */
 (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
 ;/*!
- * jQuery Cookie Plugin v1.3.1
- * https://github.com/carhartl/jquery-cookie
- *
- * Copyright 2013 Klaus Hartl
- * Released under the MIT license
- */
-(function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as anonymous module.
-    define(['jquery'], factory);
-  } else {
-    // Browser globals.
-    factory(jQuery);
-  }
-}(function ($) {
-
-  var pluses = /\+/g;
-
-  function decode(s) {
-    if (config.raw) {
-      return s;
-    }
-    try {
-      // If we can't decode the cookie, ignore it, it's unusable.
-      return decodeURIComponent(s.replace(pluses, ' '));
-    } catch(e) {}
-  }
-
-  function decodeAndParse(s) {
-    if (s.indexOf('"') === 0) {
-      // This is a quoted cookie as according to RFC2068, unescape...
-      s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-    }
-
-    s = decode(s);
-
-    try {
-      // If we can't parse the cookie, ignore it, it's unusable.
-      return config.json ? JSON.parse(s) : s;
-    } catch(e) {}
-  }
-
-  var config = $.cookie = function (key, value, options) {
-
-    // Write
-    if (value !== undefined) {
-      options = $.extend({}, config.defaults, options);
-
-      if (typeof options.expires === 'number') {
-        var days = options.expires, t = options.expires = new Date();
-        t.setDate(t.getDate() + days);
-      }
-
-      value = config.json ? JSON.stringify(value) : String(value);
-
-      return (document.cookie = [
-        config.raw ? key : encodeURIComponent(key),
-        '=',
-        config.raw ? value : encodeURIComponent(value),
-        options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-        options.path    ? '; path=' + options.path : '',
-        options.domain  ? '; domain=' + options.domain : '',
-        options.secure  ? '; secure' : ''
-      ].join(''));
-    }
-
-    // Read
-
-    var result = key ? undefined : {};
-
-    // To prevent the for loop in the first place assign an empty array
-    // in case there are no cookies at all. Also prevents odd result when
-    // calling $.cookie().
-    var cookies = document.cookie ? document.cookie.split('; ') : [];
-
-    for (var i = 0, l = cookies.length; i < l; i++) {
-      var parts = cookies[i].split('=');
-      var name = decode(parts.shift());
-      var cookie = parts.join('=');
-
-      if (key && key === name) {
-        result = decodeAndParse(cookie);
-        break;
-      }
-
-      // Prevent storing a cookie that we couldn't decode.
-      if (!key && (cookie = decodeAndParse(cookie)) !== undefined) {
-        result[name] = cookie;
-      }
-    }
-
-    return result;
-  };
-
-  config.defaults = {};
-
-  $.removeCookie = function (key, options) {
-    if ($.cookie(key) !== undefined) {
-      // Must not alter options, thus extending a fresh object...
-      $.cookie(key, '', $.extend({}, options, { expires: -1 }));
-      return true;
-    }
-    return false;
-  };
-
-}));
-;/*!
  * Smooth Scroll - v1.4.13 - 2013-11-02
  * https://github.com/kswedberg/jquery-smooth-scroll
  * Copyright (c) 2013 Karl Swedberg
@@ -231,8 +124,8 @@ $(function() {
     });
     if (isMac) {
       var $leList = $($(el).attr('href')).find('.provider-list');
-      //TODO hie doesn't have provider-list yet. It will...
-      if (!$leList.length) return;
+      // Only show the scroll-down indicator if we need it
+      if (!$leList.length || ($leList.innerHeight() > $leList[0].scrollHeight)) return;
       setTimeout(function(){
         if ($leList[0].clientHeight < $leList[0].scrollHeight) {
           $leList.next().find('.scroll-hint').removeClass('no-show');
