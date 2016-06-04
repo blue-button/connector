69518005ca4a032504ca133b6e6830262e9246dc
Jed Wood <jed@limechile.com>
2016-06-04 10:47:25 -0600

    git fetch in gh-pages build script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Build the GH-Pages site, from Travis CI
-npm run build-organizations;
-npm run build-apps;
 git config user.name ${GIT_NAME}
 git config user.email ${GIT_EMAIL}
+git fetch;
+npm run build-organizations;
+npm run build-apps;
 git status;
 git add --all;
 git commit -am "update data for one or more profiles";
