00871abb95fad34adb30d185bf847ec999587e92
Jed Wood <jed@limechile.com>
2016-06-04 10:39:37 -0600

    more attempts to get Travis CI working

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Build the site
-git pull origin master;
+# Build the GH-Pages site, from Travis CI
 npm run build-organizations;
 npm run build-apps;
+git config user.name ${GIT_NAME}
+git config user.email ${GIT_EMAIL}
 git status;
 git add --all;
 git commit -am "update data for one or more profiles";
-git push origin master;
-git subtree push --prefix public origin gh-pages;
+git subtree push --prefix=public "https://${GH_TOKEN}@${GH_REF}" gh-pages;
