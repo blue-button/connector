7a214e6a529341f1da8d25dfdb3591ad546ee8a5
Jed Wood <jed@limechile.com>
2017-01-26 08:58:23 -0700

    more debug of Travis build

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -3,10 +3,13 @@
 git config user.name ${GIT_NAME}
 git config user.email ${GIT_EMAIL}
 git fetch;
-git subtree split --prefix public -b gh-pages;
 git status;
 npm run build-organizations;
 npm run build-apps;
 git add --all;
 git commit -am "update data for one or more profiles";
+git subtree split --prefix public -b gh-pages;
+git status;
+git checkout gh-pages;
+git status;
 git push -f "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
